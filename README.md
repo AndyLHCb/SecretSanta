8e39cd907e60ee76d383a54abe13155e82f957c4
Andy Morris <Andy.Morris@warwick.ac.uk>
2018-11-29 22:27:22 +0100

    Release version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SecretSanta
 A Secret Santa for the Warwick Postgrads (1st year) in Python
+
+This is entirely written in pure python, it will work both in python 2 and python 3
---
+++ Santa.py
@@ -0,0 +1,52 @@
+import smtplib
+from getpass import getpass
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from random import choice
+
+#setting up the sending email address
+username = "Warwick.secretsanta"
+domain = "gmail.com"
+email = username + "@" + domain
+pword = getpass() #Password for the Warwick.secretsanta@gmail.com account
+
+recipients = {"Andy"   : "Andy.Morris@warwick.ac.uk",
+              "Ross"   : "Ross.Hunter@warwick.ac.uk",
+              "Flavia" : "Flavia.Cicala@warwick.ac.uk",
+              "Mousam" : "Mousam.Rai@warwick.ac.uk",
+              "Bryn"   : "Bryn.Roberts@warwick.ac.uk",
+              "Eleanor": "Eleanor.Jones.1@warwick.ac.uk"}
+recipients_len = len(recipients)
+
+#Making sure no-one gets 2 presents
+usedNames = []
+
+#Logging onto the email client
+server = smtplib.SMTP("smtp." + domain,587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username,pword)
+
+#Working out who's sending to whom
+for i in range(recipients_len):
+    sendTo = recipients[recipients.keys()[i]]
+    while(True):
+        recipientName = choice(recipients.keys())
+        if (recipients[recipientName] != sendTo) and (not (recipientName in usedNames)):
+            usedNames += [recipientName]
+            break
+
+    #Message to be sent
+    msg = MIMEMultipart()
+    msg['Subject'] = "Secret Santa"
+    
+    body = "Your secret santa is for: " + recipientName
+
+    msg.attach(MIMEText(body,'plain'))
+    text = msg.as_string()
+
+    server.sendmail(email, recipients[sender], text)
+    del msg
+
+server.quit()
